a0f983b3359b9cc0ce4b72d9cdff68e9332cbb23
Nicole Apsche <napsche@kgtiger.com>
2020-07-18 17:58:49 -0400

    creating code to call orm functions

---
+++ models/burger.js
@@ -0,0 +1,31 @@
+var orm = require("../config/orm.js");
+
+//create the code that will call ORM functions using burger 
+//specific input for the ORM
+//export at the end
+
+var burger = {
+    all: function(cb) {
+        orm.all("burgers", function(res) {
+            cb(res);
+        });
+    },
+
+    create: function(cols, vals, cb) {
+        orm.create("burgers", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+    update: function(objColVals, condition, cb) {
+        orm.update("burgers", objColVals, condition, function(res) {
+            cb(res);
+        });
+    },
+    delete: function(condition, cb) {
+        orm.delete("burgers", condition, function(res) {
+            cb(res);
+        });
+    }
+};
+
+module.exports = burger; 
