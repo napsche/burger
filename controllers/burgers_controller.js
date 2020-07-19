f42e4239d3a4ae92d3c3d80b67b3b1e68f8b3c40
Nicole Apsche <napsche@kgtiger.com>
2020-07-18 21:45:36 -0400

    adding css and editing

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,7 +5,7 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 router.get("/", function(req, res) {
-    burger.all(function(data) {
+    burger.selectAll(function(data) {
         var hbsObject = {
             burgers: data
         };
@@ -14,33 +14,47 @@ router.get("/", function(req, res) {
     });
 });
 
-router.post("/api/burgers", function(req, res) {
-    burger.create([
-        "burger_name", "devoured"
-    ], [
-        req.body.burger_name, req.body.devoured
-    ], function(result) {
-        res.json({ id: result.insertId });
+// router.post("/", function(req, res) {
+//     burger.create([
+//         "burger_name", "devoured"
+//     ], [
+//         req.body.burger_name, req.body.devoured
+//     ], function(result) {
+//         res.json({ id: result.insertId });
+//     });
+// });
+
+router.post("/", function(req, res) {
+    console.log(req.body.burger_name);
+    if(req.body.burger_name !== "") {
+        burger.insertOne(req.body.burger_name.trim(), function() {
+            res.redirect("/");
+        });
+    }
+});
+
+router.put("/:id", function(req, res) {
+    // var condition = "id = " + req.params.id; 
+
+    // console.log("condition", condition);
+
+    // burger.update({
+    //     devoured: req.body.devoured
+    // }, condition, function(result) {
+    //     if (result.changedRows == 0) {
+    //         return res.status(404).end();
+    //     } else {
+    //         res.status(200).end();
+    //     }
+    // });
+    console.log(req.params.id);
+
+    burger.updateOne(req.params.id, function() {
+        res.redirect("/");
     });
 });
 
-router.put("/api/burgers/:id", function(req, res) {
-    var condition = "id = " + req.params.id; 
-
-    console.log("condition", condition);
-
-    burger.update({
-        devoured: req.body.devoured
-    }, condition, function(result) {
-        if (result.changedRows == 0) {
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
-    });
-});
-
-router.delete("/api/burgers/:id", function(req, res) {
+router.delete("/:id", function(req, res) {
     var condition = "id = " + req.params.id; 
 
     burger.delete(condition, function(result) {
--- models/burger.js
+++ models/burger.js
@@ -5,23 +5,23 @@ var orm = require("../config/orm.js");
 //export at the end
 
 var burger = {
-    all: function(cb) {
-        orm.all("burgers", function(res) {
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res) {
             cb(res);
         });
     },
 
-    create: function(cols, vals, cb) {
-        orm.create("burgers", cols, vals, function(res) {
+    insertOne: function(burgerName, cb) {
+        orm.insertOne("burgers", "burger_name", burgerName, function(res) {
             cb(res);
         });
     },
-    update: function(objColVals, condition, cb) {
-        orm.update("burgers", objColVals, condition, function(res) {
+    updateOne: function(burgerId, cb) {
+        orm.updateOne("burgers", "devoured", 1, "id", burgerId, function(res) {
             cb(res);
         });
     },
-    delete: function(condition, cb) {
+    deleteOne: function(condition, cb) {
         orm.delete("burgers", condition, function(res) {
             cb(res);
         });
